bf3756b108b65a690d3f1521b39160a2886dc2d3
sandy <sandydwi75@gmail.com>
2025-01-17 15:57:35 +0700

    feat: search contact API

--- src/main/java/belajarspringrestfulapi/restful/controller/ContactController.java
+++ src/main/java/belajarspringrestfulapi/restful/controller/ContactController.java
@@ -1,14 +1,14 @@
 package belajarspringrestfulapi.restful.controller;
 
 import belajarspringrestfulapi.restful.entity.User;
-import belajarspringrestfulapi.restful.model.ContactResponse;
-import belajarspringrestfulapi.restful.model.CreateContactRequest;
-import belajarspringrestfulapi.restful.model.UpdateContactRequest;
-import belajarspringrestfulapi.restful.model.WebResponse;
-import belajarspringrestfulapi.restful.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import belajarspringrestfulapi.restful.model.*;
+import belajarspringrestfulapi.restful.service.ContactService;
+
+import java.util.List;
 
 @RestController
 public class ContactController {
@@ -52,5 +52,29 @@ public class ContactController {
     public WebResponse<String> delete(User user, @PathVariable("contactId") String contactId) {
         contactService.delete(user, contactId);
         return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/contacts",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<ContactResponse>> search(User user, @RequestParam(value = "name", required = false) String name, @RequestParam(value = "email", required = false) String email, @RequestParam(value = "phone", required = false) String phone, @RequestParam(value = "page", required = false, defaultValue = "0") Integer page, @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+        SearchContactRequest request = SearchContactRequest.builder()
+                .page(page)
+                .size(size)
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .build();
+
+        Page<ContactResponse> contactResponses = contactService.search(user, request);
+        return WebResponse.<List<ContactResponse>>builder()
+                .data(contactResponses.getContent())
+                .paging(PagingResponse.builder()
+                        .currentPage(contactResponses.getNumber())
+                        .totalPage(contactResponses.getTotalPages())
+                        .size(contactResponses.getSize())
+                        .build())
+                .build();
     }
 }
--- src/main/java/belajarspringrestfulapi/restful/service/ContactService.java
+++ src/main/java/belajarspringrestfulapi/restful/service/ContactService.java
@@ -4,14 +4,24 @@ import belajarspringrestfulapi.restful.entity.Contact;
 import belajarspringrestfulapi.restful.entity.User;
 import belajarspringrestfulapi.restful.model.ContactResponse;
 import belajarspringrestfulapi.restful.model.CreateContactRequest;
+import belajarspringrestfulapi.restful.model.SearchContactRequest;
 import belajarspringrestfulapi.restful.model.UpdateContactRequest;
 import belajarspringrestfulapi.restful.repository.ContactRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import jakarta.persistence.criteria.Predicate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -79,5 +89,35 @@ public class ContactService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found"));
 
         contactRepository.delete(contact);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ContactResponse> search(User user, SearchContactRequest request) {
+        Specification<Contact> specification = (root, query, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            predicates.add(builder.equal(root.get("user"), user));
+            if (Objects.nonNull(request.getName())) {
+                predicates.add(builder.or(
+                        builder.like(root.get("firstName"), "%" + request.getName() + "%"),
+                        builder.like(root.get("lastName"), "%" + request.getName() + "%")
+                ));
+            }
+            if (Objects.nonNull(request.getEmail())) {
+                predicates.add(builder.like(root.get("email"), "%" + request.getEmail() + "%"));
+            }
+            if (Objects.nonNull(request.getPhone())) {
+                predicates.add(builder.like(root.get("phone"), "%" + request.getPhone() + "%"));
+            }
+
+            return query.where(predicates.toArray(new Predicate[]{})).getRestriction();
+        };
+
+        Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
+        Page<Contact> contacts = contactRepository.findAll(specification, pageable);
+        List<ContactResponse> contactResponses = contacts.getContent().stream()
+                .map(this::toContactResponse)
+                .toList();
+
+        return new PageImpl<>(contactResponses, pageable, contacts.getTotalElements());
     }
 }
