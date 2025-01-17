f346429b4175a33413fb4ffd2f99c94b18e27926
sandy <sandydwi75@gmail.com>
2025-01-17 15:27:08 +0700

    feat: logout API

--- src/main/java/belajarspringrestfulapi/restful/controller/AuthController.java
+++ src/main/java/belajarspringrestfulapi/restful/controller/AuthController.java
@@ -1,11 +1,13 @@
 package belajarspringrestfulapi.restful.controller;
 
+import belajarspringrestfulapi.restful.entity.User;
 import belajarspringrestfulapi.restful.model.LoginUserRequest;
 import belajarspringrestfulapi.restful.model.TokenResponse;
 import belajarspringrestfulapi.restful.model.WebResponse;
 import belajarspringrestfulapi.restful.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,14 @@ public class AuthController {
     public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
         TokenResponse tokenResponse = authService.login(request);
         return WebResponse.<TokenResponse>builder().data(tokenResponse).build();
+    }
+
+    @DeleteMapping(
+            path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(User user) {
+        authService.logout(user);
+        return WebResponse.<String>builder().data("OK").build();
     }
 }
